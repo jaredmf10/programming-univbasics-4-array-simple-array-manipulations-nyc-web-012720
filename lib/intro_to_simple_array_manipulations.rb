b28b84989fc6000e5e27b42f8824119651470214
Jared Fleming <jaredmf10@gmail.com>
2020-01-13 21:46:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,3 +38,7 @@ end
 def using_flatten(instruments)
 instruments.flatten
 end
+#using_delete takes in two arguments, an array and a string, and uses the delete method to remove any items from the array that are equal to that string
+def using_delete(instructors, name)
+  instructors.delete("Steven")
+end
