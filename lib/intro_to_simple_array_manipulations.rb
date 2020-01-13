fbf4c974b21bc31e1a9a393f2b9a55436ec0ee15
Jared Fleming <jaredmf10@gmail.com>
2020-01-13 20:56:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 #using_push takes in two arguments, an array and a string and adds that string to the end of the array using the push method
-def array = ([colors_in_the_rainbow], next_color)
+def array([colors_in_the_rainbow], next_color)
 colors_in_the_rainbow = ["red", "orange", "yellow"]
 next_color = "green"
 array.push(next_color)
