79968950f72eb8bfc030e81f3f1cc2acbe160cf5
Jared Fleming <jaredmf10@gmail.com>
2020-01-13 20:56:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,8 @@
 #using_push takes in two arguments, an array and a string and adds that string to the end of the array using the push method
-def array(colors_in_the_rainbow)
+def array([colors_in_the_rainbow], next_color)
 colors_in_the_rainbow = ["red", "orange", "yellow"]
-array.push("green")
+next_color = "green"
+array.push(next_color)
 end
 #using_unshift takes in two arguments, an array and a string and adds that string to the front of the array using the unshift method and increases the length of the array
 
