bb4c53b9b593b96b92f5ac1de8b144ccc5ad07bc
Jared Fleming <jaredmf10@gmail.com>
2020-01-13 20:40:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,14 @@
-#using push
+#using_push takes in two arguments, an array and a string and adds that string to the end of the array using the push method
 def colors_in_the_rainbow([colors], next_color)
 colors = ["red", "green", "blue"]
 next_color = "orange"
 colors_in_the_rainbow.push(next_color)
 end
+
+#using_unshift takes in two arguments, an array and a string and adds that string to the front of the array using the unshift method and increases the length of the array
+
+#using_pop takes in argument of an array and uses the pop method to remove the last element from the array and return that element and decreases the length of the array by 1
+
+#pop_with_args takes in an argument of an array and uses the pop method with an argument of 2 to remove the last two array items and return them and decreases the length of the array by 2
+
+#using_shift takes in an argument of an array and uses the shift method to remove the first item and return it and decreases the length of the array by 1
